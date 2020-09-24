748b22ffa81b2eb923ed5fa75aec766a675395d4
Konstantin Bätz <33087214+konstantin-baetz@users.noreply.github.com>
2020-09-24 20:30:31 +0200

    Update benchmark_model.py

--- projects/prediction_competition/benchmark_model.py
+++ projects/prediction_competition/benchmark_model.py
@@ -59,7 +59,7 @@ log = logging.getLogger(__name__)
 
 # Do you wish to fetch the latest public data? If so, change False to True and run this cell
 # Cells below will fail if this is not run if you haven't imported data yourself yet.
-if True:
+if False:
     path_zip = views.apps.data.public.fetch_latest_zip_from_website(path_dir_destination=views.DIR_SCRATCH)
     views.apps.data.public.import_tables_and_geoms(tables=views.TABLES, geometries=views.GEOMETRIES, path_zip=path_zip)
 
