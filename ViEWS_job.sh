03da15c83080561e8aef6fc9df4ad9473bf04d4b
Konstantin Bätz <33087214+konstantin-baetz@users.noreply.github.com>
2020-09-24 16:21:21 +0200

    Add files via upload

---
+++ ViEWS_job.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+conda initiate
+conda activate views2 
+
+echo "loading data"
+
+python runners/import_data.py --fetch
+
+echo "starting script"
+
+python projects/prediction_competition/konstanz_models.py
+
+echo "estimations sucessful"
