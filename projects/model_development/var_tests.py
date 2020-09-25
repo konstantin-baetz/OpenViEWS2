47dc657c569a3e994da4b1e5b7108fe25a0804d3
Konstantin Bätz <33087214+konstantin-baetz@users.noreply.github.com>
2020-09-25 21:33:40 +0200

    Update var_tests.py

--- projects/model_development/var_tests.py
+++ projects/model_development/var_tests.py
@@ -30,10 +30,8 @@ log = logging.getLogger(__name__)
 from views.utils import io
 path = "~/OpenViEWS2/storage/data/datasets/manual.parquet"  # change to your path
 cm_global_imp_0 = io.parquet_to_df(path)
-dataset = cm_global_imp_0
+df = cm_global_imp_0
 
-
-df = dataset.df
 vars = list(df.columns.values)
 print(*vars, sep = "\n")
 #var = tlag_8_ged_dummy_sb
