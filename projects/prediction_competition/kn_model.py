559decc0cfaa119c542d304083b38b420b65dbab
Konstantin Bätz <33087214+konstantin-baetz@users.noreply.github.com>
2020-09-27 15:08:07 +0200

    Update kn_model.py

--- projects/prediction_competition/kn_model.py
+++ projects/prediction_competition/kn_model.py
@@ -1080,7 +1080,7 @@ model_d3 = api.Model(
 # Lists of models are convenient
 #models = [model_0, model_1, model_2, model_3]
 delta_models = [model_d0, model_d1, model_d2, model_d3]
-models = [model_2]
+models = [model_baseline]
 
 
 avg_ensemble = Ensemble(
