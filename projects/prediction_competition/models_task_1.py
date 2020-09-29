6048ba6fb89b6d19016bff1687910b1ed0abd757
Konstantin Bätz <33087214+konstantin-baetz@users.noreply.github.com>
2020-09-29 17:53:36 +0200

    Update models_task_1.py

--- projects/prediction_competition/models_task_1.py
+++ projects/prediction_competition/models_task_1.py
@@ -1016,7 +1016,7 @@ model_3 = api.Model(
 
 # Lists of models are convenient
 #models = [model_0, model_1, model_2, model_3]
-models = [model_3]
+models = [model_0]
 #models = [model_baseline]
 # Train all models
 for model in models:
