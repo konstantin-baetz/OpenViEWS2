6e52477889c30e59d50e159ffce20ec5a4429e6b
Konstantin Bätz <33087214+konstantin-baetz@users.noreply.github.com>
2020-09-30 18:40:19 +0200

    Update models_task_1.py

--- projects/prediction_competition/models_task_1.py
+++ projects/prediction_competition/models_task_1.py
@@ -174,6 +174,10 @@ structural_variables = [
     "kn_food_price"]
 
 political_variables = [
+    "kn_relative_age",
+    "kn_leader_age"]
+
+political_variables_full = [
     "reign_anticipation",
     "reign_couprisk",
     "reign_delayed",
@@ -199,7 +203,7 @@ single_variable = ["time_since_splag_1_1_acled_dummy_ns"]
 features_test = test_features
 features_0 = basic_features + mdums + cdums
 features_1 = basic_features + mdums + cdums + structural_variables + corona_variables 
-features_2 = basic_features + mdums + cdums + structural_variables + corona_variables  + political_variables
+features_2 = political_variables
 features_3 = basic_features + mdums + cdums + structural_variables + corona_variables + political_variables + survey_variables 
 #features_4 = basic_features + mdums + cdums + structural_variables 
 
@@ -303,7 +307,7 @@ model_d3 = api.Model(
 
 
 # Lists of models are convenient
-models = [model_1, model_2, model_3]
+models = [model_2, model_3]
 models_d = [model_d0, model_d1, model_d2, model_d3]
 #models = [model_1]
 #models = [model_baseline]
